c40c3630feb9b851fc871c13b501cf4ee8d13608
empis <master@empis.eu>
2015-08-14 00:08:41 +0200

    remove arrow

--- .scripts/dzen_menu.sh
+++ .scripts/dzen_menu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -i
 source $(dirname $0)/bar/config.sh
 
-(echo -e "Menu\n^ca(1,power suspend)Uspat^ca()\n^ca(1,power hibernate)Hibernovat^ca()\n^ca(1,power off)Vypnút^ca()\n^ca(1,power restart)Restartovat^ca()\n^ca(1,"scrun dzen_services.sh")Sluzby ->^ca()"; sleep 15) \
+(echo -e "Menu\n^ca(1,power suspend)Uspat^ca()\n^ca(1,power hibernate)Hibernovat^ca()\n^ca(1,power off)Vypnút^ca()\n^ca(1,power restart)Restartovat^ca()\n^ca(1,"scrun dzen_services.sh")Sluzby^ca()"; sleep 15) \
 | dzen2 -x 0 -y 0 -w 100 -bg $background -fg $foreground -fn $FONT -h 14 -sa c -l 5 -e 'onstart=uncollapse,hide' -m v 
